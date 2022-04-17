2ced53a2946c5a96361198b3e3f2c9e89b948df9
Alexey Peresypkin <aepre3@gmail.com>
2022-04-17 09:21:45 +0300

    Created test_user_edit.py

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -10,6 +10,30 @@ class TestUserGet(BaseCase):
     def test_get_user_details_not_auth(self):
         url = 'https://playground.learnqa.ru/api/user/2'
         response = requests.get(url)
-        print(response.content)
 
-    assert 1 == 1
+        Assertions.asser_json_has_key(response, 'username')
+        Assertions.asser_json_has_not_key(response, 'email')
+        Assertions.asser_json_has_not_key(response, 'firstName')
+        Assertions.asser_json_has_not_key(response, 'lastName')
+
+    def test_get_user_details_auth_as_same_user(self):
+        url = 'https://playground.learnqa.ru/api/user/login'
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response = requests.post(url, data=data)
+        auth_sid = self.get_cookie(response, 'auth_sid')
+        token = self.get_header(response, 'x-csrf-token')
+        user_id_from_auth_method = self.get_json_value(response, 'user_id')
+
+        response2 = requests.get(
+            f'https://playground.learnqa.ru/api/user/{user_id_from_auth_method}',
+            headers={'x-csrf-token': token},
+            cookies={'auth_sid': auth_sid}
+        )
+
+        Assertions.asser_json_has_keys(
+            response2,
+            ['username', 'email', 'firstName', 'lastName']
+        )
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -8,35 +8,19 @@ from lib.base_case import BaseCase
 
 class TestUserRegister(BaseCase):
 
-    def setup(self):
-        base_part = 'learnqa'
-        domain = '@example.com'
-        random_part = datetime.now().strftime('%m%d%Y%H%M%S')
-        self.email = f'{base_part}{random_part}{domain}'
-
     def test_create_user_successfully(self):
         url = 'https://playground.learnqa.ru/api/user/'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
+        data = self.prepare_registration_data()
+
         response = requests.post(url, data=data)
+
         Assertions.assert_code_status(response, 200)
         Assertions.asser_json_has_key(response, 'id')
 
     def test_create_user_with_existing_email(self):
         url = 'https://playground.learnqa.ru/api/user/'
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepare_registration_data(email=email)
 
         response = requests.post(url, data=data)
         Assertions.assert_code_status(response, 400)
