5af3964437d14316ef2254d0d96645ed1efa52f8
Alexey Peresypkin <aepre3@gmail.com>
2022-04-18 21:07:45 +0300

    Created test_edit_user_without_login in TestUserEdit

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -4,17 +4,20 @@ from lib.my_requests import MyRequests
 
 
 class TestUserEdit(BaseCase):
-    def test_edit_just_crated_user(self):
+
+    def setup(self):
+        self.register_data_1 = self.prepare_registration_data()
+        self.register_data_2 = self.prepare_registration_data()
+
+    def test_edit_just_created_user(self):
         # REGISTER
-        register_data = self.prepare_registration_data()
-        response1 = MyRequests.post('/user', data=register_data)
+        response1 = MyRequests.post('/user', data=self.register_data_1)
 
         Assertions.assert_code_status(response1, 200)
         Assertions.asser_json_has_key(response1, 'id')
 
-        email = register_data['email']
-        first_name = register_data['firstName']
-        password = register_data['password']
+        email = self.register_data_1['email']
+        password = self.register_data_1['password']
         user_id = self.get_json_value(response1, 'id')
 
         # LOGIN
@@ -51,3 +54,14 @@ class TestUserEdit(BaseCase):
             new_name,
             'Wrong name of the user after edit'
         )
+
+    def test_edit_user_without_login(self):
+        response1 = MyRequests.post('/user', data=self.register_data_1)
+        user_id = self.get_json_value(response1, 'id')
+        new_name = 'ChangedName'
+        response2 = MyRequests.put(
+            f'/user/{user_id}',
+            data={'firstName': new_name}
+        )
+        Assertions.assert_code_status(response2, 400)
+        assert response2.content.decode('utf-8') == f'Auth token not supplied', f'Unexpected response content {response2.content}'
