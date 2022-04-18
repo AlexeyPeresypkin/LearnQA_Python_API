9cfc10336f85a0ef9fd28d989d72cbfe3fd83b48
Alexey Peresypkin <aepre3@gmail.com>
2022-04-18 07:03:25 +0300

    Created test_get_user_details_auth_as_other_user in TestUserGet

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -33,3 +33,22 @@ class TestUserGet(BaseCase):
             response2,
             ['username', 'email', 'firstName', 'lastName']
         )
+
+    def test_get_user_details_auth_as_other_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response = MyRequests.post('/user/login', data=data)
+        auth_sid = self.get_cookie(response, 'auth_sid')
+        token = self.get_header(response, 'x-csrf-token')
+
+        response2 = MyRequests.get(
+            f'/user/1',
+            headers={'x-csrf-token': token},
+            cookies={'auth_sid': auth_sid}
+        )
+        Assertions.asser_json_has_key(response2, 'username')
+        Assertions.asser_json_has_not_key(response2, 'email')
+        Assertions.asser_json_has_not_key(response2, 'firstName')
+        Assertions.asser_json_has_not_key(response2, 'lastName')
